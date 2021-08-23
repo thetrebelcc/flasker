27a06dc475bc30c83e5f09bc78e41593d244c37a
Fabian <fabiananguiano@gmail.com>
2021-08-23 21:23:29 +0000

    work

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, redirect
+import time
 
 from .forms import SampleForm
 
@@ -22,6 +23,11 @@ def forms():
 
     # If form is upon submission and is validated success
     if form.validate_on_submit():
-        return redirect('index')
+        name = form.firstName.data
+        last = form.lastName.data
+
+        print(name)
+        print(last)
+        return render_template('results.html', testing = name)
 
     return render_template('forms.html', form=form)
